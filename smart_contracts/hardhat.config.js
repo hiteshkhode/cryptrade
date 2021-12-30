ca1fcd76667baee1e71812016b755b5fd8d7a675
hiteshkhode <khodehitesh@gmail.com>
2021-12-31 04:55:33 +0530

    Transactions deployed successfully

--- smart_contracts/hardhat.config.js
+++ smart_contracts/hardhat.config.js
@@ -1,21 +1,14 @@
-require("@nomiclabs/hardhat-waffle");
+// https://eth-ropsten.alchemyapi.io/v2/dKAwFEnq2COdl5GNDrDVpmERmLZGUvBJ
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+require('@nomiclabs/hardhat-waffle');
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: '0.8.4',
+  networks: {
+    ropsten: {
+      url: 'https://eth-ropsten.alchemyapi.io/v2/dKAwFEnq2COdl5GNDrDVpmERmLZGUvBJ',
+      accounts: ['0711be6a907af13ffefd04256cc004ca425c51f239a2c20891f841e3829248b1']
+
+    }
+  }
 };
